d53ab1ac441c28d89b96046973c18c4153b70790
raj-83 <rajeevranjanpusa2003@gmail.com>
2025-09-24 04:14:27 +0530

    Refactor App component to implement stack navigation with Home, Profile, and Search screens

--- App.jsx
+++ App.jsx
@@ -1,16 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Home from './src/screens/Home'
+import Profile from './src/screens/Profile'
+import Search from './src/screens/Search'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 
 const stack = createNativeStackNavigator();
 
+const StackNavigator = () => {
+  return(
+    <stack.Navigator>
+      <stack.Screen name="Home" component={Home} />
+     <stack.Screen name="Profile" component={Profile} /> 
+       <stack.Screen name="Search" component={Search} /> 
+    </stack.Navigator>
+  )
+}
+
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-      <Home />
-
-    </View>
+     <NavigationContainer>
+     <StackNavigator />
+     </NavigationContainer> 
   )
 }
 
