4860afeb826022a7fef4e0706f545d2f79bc071f
raj-83 <rajeevranjanpusa2003@gmail.com>
2025-09-24 04:35:10 +0530

    Update Home and Profile components to enable navigation between screens

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,12 +2,12 @@ import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Profile from './Profile'
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View style={{width:"100%", height:"100%", justifyContent:"center", alignItems:"center"}}>
 
         <Text style ={{fontSize:20,fontWeight:"500",marginBottom:10}}>Home</Text>
-        <Button title="Profile" onPress={()=> <Profile />}/>
+        <Button title="Profile" onPress={()=>navigation.navigate("Profile")}/>
     </View>
   )
 }
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet,Button, Text, View } from 'react-native'
 import React from 'react'
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{width:"100%", height:"100%", justifyContent:"center", alignItems:"center"}}>
+      <Text style ={{fontSize:20,fontWeight:"500",marginBottom:10}}>Profile</Text>
+      <Button title="Search" onPress={()=>navigation.navigate("Search")}/>
     </View>
   )
 }
